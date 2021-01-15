7e1612db9adb2acc063479470adb19c78c45724d
cledson <cledman@gmail.com>
2021-01-15 15:22:55 -0300

    Add option to delete vehicles from parking.

---
+++ README.md
@@ -0,0 +1,50 @@
+# Estacionamento
+
+This project is part from the course from [Digital Innovation One](https://web.digitalinnovation.one/) to study [Typescript](https://www.typescriptlang.org/).
+
+<p align="center">
+    <img src=".github/preview.png"/>
+</p>
+
+---
+
+# Guide
+
+- [Features](#features)
+- [Installation](#installation)
+- [Using](#using)
+- [Technologies](#technologies)
+- [Future Implementations](#future-implementations)
+
+# Features
+
+- We can add and remove vehicles from a parking.
+- Uses HTML Javascript
+
+# Installation
+
+`git clone https://github.com/cledman/typescript-estacionamento`
+
+# Using
+
+***before using***, type on terminal from your folder: `npx -p typescript tsc`
+
+`Run the index.html file`
+
+# Technologies
+
+- [HTML](https://www.w3schools.com/html/) to write the page,
+- [Bootstrap](https://getbootstrap.com/) to handle styles too.
+
+# Future Implementations
+
+- We could have more elements.
+- Add [CSS](https://www.w3schools.com/css/) to handle the styles, layout and rendering,
+- Better work on UX.
+
+
+Thanks to:
+
+- [Andre Soares](https://github.com/soaresderik)
+
+##
--- script.ts
+++ script.ts
@@ -1,11 +1,18 @@
 interface Veiculo {
     nome: string;
     placa: string;
-    entrada: Date;
+    entrada: Date | string;
 }
 
 (function() {
     const $ = (query: string): HTMLInputElement | null => document.querySelector(query);
+
+    function calcTempo(mil: number){
+        const min = Math.floor(mil / 60000);
+        const sec = Math.floor((mil % 60000) /1000);
+
+        return `${min}m e ${sec}s`;
+    }
 
     function patio() {
         function ler(): Veiculo[]{
@@ -18,21 +25,32 @@ interface Veiculo {
 
         function adicionar(veiculo: Veiculo, salva?: boolean){
             const row = document.createElement("tr");
+            
             row.innerHTML = `
                 <td>${veiculo.nome}</td>
                 <td>${veiculo.placa}</td>
                 <td>${veiculo.entrada}</td>
                 <td>
-                    <button class="delete" data-placa="${veiculo.placa}">x</button>
+                    <button class="delete btn btn-warning" data-placa="${veiculo.placa}">x</button>
                 </td>                                    
             `;
+
+            row.querySelector(".delete")?.addEventListener("click", function() {
+                remover(this.dataset.placa);
+            })
 
             $("#patio")?.appendChild(row);
             if (salva) salvar([...ler(), veiculo]);
         }
 
-        function remover(){
+        function remover(placa: string){
+            const { entrada, nome} = ler().find(veiculo => veiculo.placa === placa);
+            const tempo = calcTempo(new Date().getTime() - new Date(entrada).getTime());
 
+
+            if(!confirm(`O veículo ${nome} permaneceu por ${tempo}. Deseja encerrar?`)) return;
+            salvar(ler().filter(veiculo => veiculo.placa !== placa));
+            render();
         }
 
         function render(){
@@ -57,7 +75,7 @@ interface Veiculo {
             return;
         }
 
-        patio().adicionar({ nome, placa, entrada: new Date() }, true);
+        patio().adicionar({ nome, placa, entrada: new Date().toISOString() }, true);
 
     });
 }) ();
